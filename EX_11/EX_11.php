85710c597c7f7ad20a4d9a7f89a4e3fc3167ff8e
Ramonet22400 <ramonperez22400@gmail.com>
2022-09-21 20:03:46 +0200

    Segundo ejercicio PHP

--- EX_11/EX_11.php
+++ EX_11/EX_11.php
@@ -3,5 +3,19 @@
 //Crea una función que compruebe si todos los caracteres del string se encuentran en
 //mayúsculas.
 
-$frase = "Hola muy buenas a todo el mundo";
-echo($frase);
+function ma($texto){
+
+    if($texto==strtoupper($texto)){
+
+        echo("La cadena contiene mayusculas.");
+
+    } else {
+
+        echo("La cadena no contiene mayusculas.");
+    }
+
+}
+
+$texto = "HOLA";
+
+ma($texto);
--- EX_12/EX_12.php
+++ EX_12/EX_12.php
@@ -1,1 +1,19 @@
 <?php
+
+function primus ($numero){
+
+    $resultado = $numero / 2;
+
+    if($resultado == is_int($resultado)){
+
+        echo ("El numero no es primo");
+
+    }else {
+
+        echo("El numero es primo");
+    }
+}
+
+$numero = 5;
+
+primus($numero);
